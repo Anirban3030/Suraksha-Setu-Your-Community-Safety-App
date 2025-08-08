b2b594bba1a62a8470366ef2756b53666741d75e
Anirban Mondal <112965065+Anirban3030@users.noreply.github.com>
2025-08-08 09:13:06 +0530

    Update firebase_config.py

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,13 +1,18 @@
-import os, json
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
 
 load_dotenv()
-# Initialize Firebase Admin SDK
-firebase_creds = os.getenv("serviceAccountKey.json")
-cred = credentials.Certificate(firebase_creds)
-firebase_admin.initialize_app(cred)
 
-# Get Firestore client
+if not firebase_admin._apps:
+    try:
+        firebase_creds = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
+        cred = credentials.Certificate(firebase_creds)
+        firebase_admin.initialize_app(cred)
+    except Exception as e:
+        print(f"❌ Firebase initialization failed: {e}")
+        raise e
+
 db = firestore.client()
